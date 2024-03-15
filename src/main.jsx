9da59eadd6ad8595276ae1c428478a7c01af6c83
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-03-15 17:33:28 -0500

    se crea componente home

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import 'bootstrap/dis/css/bootstrap.min.css'
+import 'bootstrap/dis/js/bootstrap.min.js'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+  </React.StrictMode>,
+)
